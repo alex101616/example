afea4b3134ad283395d47a4254a404e482a5e3fe
Alex Leiva <aleiva.vrgroup@bicevida.cl>
2026-01-12 20:12:18 -0300

    ajustes

---
+++ app/routes/api.ventas.ts
@@ -0,0 +1,74 @@
+import type { LoaderFunctionArgs } from "react-router";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { admin } = await authenticate.admin(request);
+
+  const query = `
+    query GetOrdersWithCustomers(
+      $first: Int,
+      $sortKey: OrderSortKeys,
+      $reverse: Boolean
+    ) {
+      orders(first: $first, sortKey: $sortKey, reverse: $reverse) {
+        edges {
+          node {
+            id
+            name
+            createdAt
+            totalPriceSet {
+              shopMoney {
+                amount
+                currencyCode
+              }
+            }
+            metafields(first: 20) {
+              edges {
+                node {
+                  namespace
+                  key
+                  type
+                  value
+                }
+              }
+            }
+            customer {
+              id
+              displayName
+              email
+              phone
+              metafields(first: 20) {
+                edges {
+                  node {
+                    namespace
+                    key
+                    type
+                    value
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    first: 50,
+    sortKey: "CREATED_AT",
+    reverse: true,
+  };
+
+  const response = await admin.graphql(query, { variables });
+  const data = await response.json();
+  console.log(response);
+  return new Response(
+    JSON.stringify({ result: data.data }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
---
+++ app/routes/app.ventas.tsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+
+
+export default function SalesPage() {
+
+
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/ventas", {
+        method: "GET",
+      });
+
+      const json = await res.json();
+     // const orders = json.result.data.orders.edges;
+      const orders = json.result.orders.edges;
+
+      processOrders(orders);
+    };
+
+    fetchData();
+  }, []);
+
+  const processOrders = (orders) => {
+    const map = {};
+
+    orders.forEach(({ node }) => {
+      const total = parseFloat(node.totalPriceSet.shopMoney.amount);
+
+      const sapField = node.customer?.metafields.edges.find(
+        (m) => m.node.key === "id_custumer_sap"
+      );
+
+       const sapNombreField = node.customer?.metafields.edges.find(
+        (m) => m.node.key === "company"
+      );
+
+      if (!sapField) return;
+
+      const sapId = sapField.node.value;
+
+      const nombreCompany = sapNombreField.node.value;
+
+      if (!map[sapId]) {
+        map[sapId] = {
+          empresa: nombreCompany,
+          total: 0,
+        };
+      }
+
+      map[sapId].total += total;
+    });
+
+    setRows(Object.values(map));
+  };
+
+  return (
+    <>
+      <s-page heading="Reporte de Ventas por Empresa" />
+
+      <s-section padding="none">
+        <s-table>
+          <s-table-header-row>
+            <s-table-header>Empresa (SAP ID)</s-table-header>
+            <s-table-header>Total Ventas</s-table-header>
+          </s-table-header-row>
+
+          <s-table-body>
+            {rows.map((row, index) => (
+              <s-table-row key={index}>
+                <s-table-cell>{row.empresa}</s-table-cell>
+                <s-table-cell>{row.total.toFixed(2)}</s-table-cell>
+
+              </s-table-row>
+            ))}
+          </s-table-body>
+        </s-table>
+      </s-section>
+    </>
+  );
+}
